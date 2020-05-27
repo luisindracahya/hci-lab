165618bf1fdde260d4b5fa0c513e477139536fad
Stephen <stephen.leonardo@binus.edu>
2020-05-27 21:14:36 +0700

    progress

--- Scripts/layout.js
+++ Scripts/layout.js
@@ -1,15 +1,27 @@
 
 $(document).ready(function () {
 
-    $('html').click(function() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.body.style.backgroundColor = "white";
-    });
+    // $('html').click(function() {
+    //     document.getElementById("mySidenav").style.width = "0";
+    //     document.body.style.backgroundColor = "white";
+    // });
 
 
-    $('#menu_dropdown').click((e) =>{
-        e.stopPropagation(); 
-        document.getElementById("mySidenav").style.width = "220px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // $('#menu_dropdown').click((e) =>{
+    //     e.stopPropagation(); 
+    //     document.getElementById("mySidenav").style.width = "220px";
+    //     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // })
+
+
+    $('input[type="date"]').click(function(e) {
+        // this.click()
+        console.log(this)
+        document.getElementById(this.id).click();
     })
+
+    
+
+
+
 })
