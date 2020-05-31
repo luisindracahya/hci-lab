02b56b072a884f9df36650c7953bf31ea315728e
Stephen <stephen.leonardo@binus.edu>
2020-05-31 14:20:35 +0700

    whatevs

--- Scripts/home.js
+++ Scripts/home.js
@@ -13,8 +13,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = $(".mySlides");            /*************    Jquery  ************* */
+  var dots = $(".dot");            /*************    Jquery  ************* */
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
