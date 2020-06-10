869fe2aa21b004f07c1e6d8314cfe9fd6bd3cb08
FlerkenFury <60496179+FlerkenFury@users.noreply.github.com>
2020-06-11 03:38:21 +0700

    bersihkan comment
    just in case

--- Kelompok8/Scripts/layout.js
+++ Kelompok8/Scripts/layout.js
@@ -1,21 +1,7 @@
 
 $(document).ready(function () {
 
-    // $('html').click(function() {
-    //     document.getElementById("mySidenav").style.width = "0";
-    //     document.body.style.backgroundColor = "white";
-    // });
-
-
-    // $('#menu_dropdown').click((e) =>{
-    //     e.stopPropagation(); 
-    //     document.getElementById("mySidenav").style.width = "220px";
-    //     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    // })
-
-
     $('input[type="date"]').click(function(e) {
-        // this.click()
         console.log(this)
         document.getElementById(this.id).click();
     })
